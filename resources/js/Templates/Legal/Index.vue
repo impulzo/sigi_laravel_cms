c9450ee6fe5b99b1d00836cba91c1f8cbe9524b5
Jose Santiago figueroa <josesantiagofigueroa@MacBook-Air-de-Jose.local>
2021-10-07 16:30:54 -0500

    Add Packages

--- resources/js/Templates/Legal/Index.vue
+++ resources/js/Templates/Legal/Index.vue
@@ -225,72 +225,16 @@
     <!-- End Portfolio Section -->
 
     <!-- ======= Pricing Section ======= -->
-    <!-- <section id="pricing" class="pricing">
+    <section id="pricing" class="pricing">
       <div class="container">
-
         <div class="section-title">
           <span>Pricing</span>
           <h2>Pricing</h2>
           <p>Sit sint consectetur velit quisquam cupiditate impedit suscipit alias</p>
         </div>
-
-        <div class="row">
-
-          <div class="col-lg-4 col-md-6" data-aos="zoom-in" data-aos-delay="150">
-            <div class="box">
-              <h3>Free</h3>
-              <h4><sup>$</sup>0<span> / month</span></h4>
-              <ul>
-                <li>Aida dere</li>
-                <li>Nec feugiat nisl</li>
-                <li>Nulla at volutpat dola</li>
-                <li class="na">Pharetra massa</li>
-                <li class="na">Massa ultricies mi</li>
-              </ul>
-              <div class="btn-wrap">
-                <a href="#" class="btn-buy">Buy Now</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 mt-4 mt-md-0" data-aos="zoom-in">
-            <div class="box featured">
-              <h3>Business</h3>
-              <h4><sup>$</sup>19<span> / month</span></h4>
-              <ul>
-                <li>Aida dere</li>
-                <li>Nec feugiat nisl</li>
-                <li>Nulla at volutpat dola</li>
-                <li>Pharetra massa</li>
-                <li class="na">Massa ultricies mi</li>
-              </ul>
-              <div class="btn-wrap">
-                <a href="#" class="btn-buy">Buy Now</a>
-              </div>
-            </div>
-          </div>
-
-          <div class="col-lg-4 col-md-6 mt-4 mt-lg-0" data-aos="zoom-in" data-aos-delay="150">
-            <div class="box">
-              <h3>Developer</h3>
-              <h4><sup>$</sup>29<span> / month</span></h4>
-              <ul>
-                <li>Aida dere</li>
-                <li>Nec feugiat nisl</li>
-                <li>Nulla at volutpat dola</li>
-                <li>Pharetra massa</li>
-                <li>Massa ultricies mi</li>
-              </ul>
-              <div class="btn-wrap">
-                <a href="#" class="btn-buy">Buy Now</a>
-              </div>
-            </div>
-          </div>
-
-        </div>
-
+        <package :component="getComponentId('packages')"></package>
       </div>
-    </section> -->
+    </section>
     <!-- End Pricing Section -->
 
     <!-- ======= Team Section ======= -->
@@ -301,9 +245,7 @@
           <h2>Equipo</h2>
           <p>Ninguno de nosotros es tan bueno como todos nosotros juntos. Ray Kroc.</p>
         </div>
-        <!-- <div class="row"> -->
-          <team :component="getComponentId('team')" ></team>
-        <!-- </div> -->
+        <team :component="getComponentId('team')" ></team>
       </div>
     </section>
     <!-- End Team Section -->
@@ -444,6 +386,7 @@
 </template>
 
 <script>
+import Package from './Components/Package.vue'
 import About from './Components/About.vue'
 import Service from './Components/Service.vue'
 import Titlen from './Components/Title.vue'
@@ -462,7 +405,8 @@ export default {
     Mission, 
     Titlen,
     About, 
-    Service },
+    Service,
+    Package },
   name: 'Index Legal Template',
   props: ['template'],
   data:function(){
@@ -487,6 +431,7 @@ export default {
     }
   },
   components:{
+    Package,
     About,
     Contact,
     Mission,
